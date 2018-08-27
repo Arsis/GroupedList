f6ca1f9c75b57b5fdedc7c661c416006fda8dbf0
Arsis <te-n-et@yandex.ru>
2018-08-27 14:59:03 +0300

    Initial Commit

---
+++ GroupedList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupedList
+//
+//  Created by DF on 8/27/18.
+//  Copyright © 2018 Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
